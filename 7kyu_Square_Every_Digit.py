0524530b8f57b20a38d1959a52489a5b548dc4c0
mns <mns@mns-PC>
2019-04-14 12:49:21 +0300

    7ky python

---
+++ 7kyu_Square_Every_Digit.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/square-every-digit/train/python
+
+#Welcome. In this kata, you are asked to square every digit of a number.
+
+#For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+#Note: The function accepts an integer and returns an integer
+
+
+
+def square_digits(num):
+
+    num = str(num)
+    answ = ""
+    for n in num:
+        answ += str(int(n)*int(n))
+    return int(answ)
