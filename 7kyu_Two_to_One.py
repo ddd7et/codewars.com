1fd47c0044cbe9a9cb0ecfe8236730530643fb57
mns <mns@mns-PC>
2019-04-18 09:53:00 +0300

    7ky python

---
+++ 7kyu_Two_to_One.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/two-to-one/python
+#Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+#each taken only once - coming from s1 or s2.
+#Examples:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+def longest(s1, s2):
+    return "".join(sorted(set
