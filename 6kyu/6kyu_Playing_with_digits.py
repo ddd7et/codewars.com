55d13286833ca9b1005e409af7ef1df8b7667d02
ddd7et <ddd8et@tutanota.de>
2019-07-14 15:08:52 +0300

    6kyu

--- 6kyu/6kyu_Playing_with_digits.py
+++ 6kyu/6kyu_Playing_with_digits.py
@@ -1,22 +1,34 @@
+# https://www.codewars.com/kata/playing-with-digits/python
+# Some numbers have funny properties. For example:
+
+#    89 --> 8¹ + 9² = 89 * 1
+
+#    695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+#    46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+#Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+#    we want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive powers of p is equal to k * n.
+
+#In other words:
+
+#    Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+#If it is the case we will return k, if not return -1.
+
+#Note: n and p will always be given as strictly positive integers.
+
 import math
 def dig_pow(n, p):
     # your code
-    return -1
+    n = str(n)
+    sum = 0
+    for i, s in enumerate(n):
+    	#print("sum=", s)
+        sum += pow(int(s), i+p)
 
-answer = dig_pow(89, 1)#, 1
-answer = dig_pow(92, 1)# -1
-answer = dig_pow(46288, 3)# , 51
-
-print(answer)
-
-numb = 46288
-
-str = str(numb)
-k = 3
-sum = 0
-
-for i, s in enumerate(str):
-	pass
-	#print("sum=", (i+k)*ma
-
-print(math.pow(2,3))
+    if sum/int(n) %1 ==0.0:
+        return sum/int(n)
+    else:
+        return -1
