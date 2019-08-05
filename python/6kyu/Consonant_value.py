9f435e52076a5dcf75a7a9e9ea25d1c37fec8da4
ddd7et <ddd8et@tutanota.de>
2019-08-05 14:32:51 +0300

    add 6kyu from codewars.com

---
+++ python/6kyu/Consonant_value.py
@@ -0,0 +1,28 @@
+#https://www.codewars.com/kata/consonant-value/train/python
+
+#Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alpahabet except "aeiou".
+
+#We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+#For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+
+#-- The consonant substrings are: "z", "d" and "cs" and the values are z = 26, d = 4 and cs = 3 + 19 = 22. The highest is 26.
+#solve("zodiacs") = 26
+
+#For the word "strength", solve("strength") = 57
+#-- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
+
+#For C: do not mutate input.
+
+def solve(s):
+    volwes = "aeiou"
+    max = 0
+    sum = 0
+    for c in s:
+	    if not c in volwes:
+		    sum += ord(c)-96
+		    if sum > max:
+			    max = sum
+	    else:
+		    sum = 0
+    return max
